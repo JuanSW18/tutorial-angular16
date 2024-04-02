13ec0dc8af0fe071b860907e16eadfb749e30fa1
Juan Eneque <jneneque@gmail.com>
2024-04-01 21:54:06 -0500

    shared module & basic material components

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+const ANGULAR_MATERIAL_COMPONENTS = [
+  MatButtonModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatFormFieldModule,
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...ANGULAR_MATERIAL_COMPONENTS,
+  ],
+  exports: [
+    ...ANGULAR_MATERIAL_COMPONENTS,
+  ]
+})
+export class SharedModule { }
